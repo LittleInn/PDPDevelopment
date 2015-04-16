c854d202468f6b3f37c14ebdbcdf2e6d43d8d818
user <user@home>
2015-04-17 00:21:05 +0300

    Add logger (log4j)

--- development/src/main/java/com/pdp/reflection/development/deepcopy/RunCopyApp.java
+++ development/src/main/java/com/pdp/reflection/development/deepcopy/RunCopyApp.java
@@ -5,8 +5,8 @@ import org.apache.log4j.Logger;
 public class RunCopyApp {
 	private static Logger LOGGER = Logger.getLogger(RunCopyApp.class);
 	public static void main(String[] args) {
-//		User user1 = new User("Alex", "alex@mail.com", "IE4523");
-//		User user2 = new User("John", "john90@mail.com", "A49023");
+		User user1 = new User("Alex", "alex@mail.com", "IE4523");
+		User user2 = new User("John", "john90@mail.com", "A49023");
 		Address address1 = new Address("Lviv", "78439");
 		Address address2 = new Address("Kyiv", "12355");
 		Employee employee1 = new Employee("Emma", "emm00@mail.com", "HJ9012",
@@ -14,15 +14,14 @@ public class RunCopyApp {
 		Employee employee2 = new Employee("Keit", "keit@mail.com", "OP9122",
 				940.00, "A95", "developer", address2);
 
-//		System.out.println("User1: " + user1);
-//		System.out.println("User2: " + user2);
-		System.out.println("Employee1: " + employee1);
-		System.out.println("Employee2: " + employee2);
-//		DeepCopy.copy(user1, user2);
+		LOGGER.info("Employee1: " + employee1);
+		LOGGER.info("Employee2: " + employee2);
+		DeepCopy.copy(user1, user2);
 		DeepCopy.copy(employee1, employee2);
-//		System.out.println("User1: " + user1);
-//		System.out.println("User2: " + user2);
-		System.out.println("Employee1: " + employee1);
-		System.out.println("Employee2: " + employee2);
+		LOGGER.info("Objects after copy");
+		LOGGER.info("Employee1: " + employee1);
+		LOGGER.info("Employee2: " + employee2);
+		LOGGER.info("Employee1 zip code: " + employee1.getAddress().getZipCode());
+		LOGGER.info("Employee2 zip code: " + employee2.getAddress().getZipCode());
 	}
 }
