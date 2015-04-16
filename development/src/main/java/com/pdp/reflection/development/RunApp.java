01b6361c04230441bd02ef6d582dd32b6c059775
user <user@home>
2015-04-17 00:04:00 +0300

    Delete unused class

--- development/src/main/java/com/pdp/reflection/development/RunApp.java
+++
@@ -1,8 +0,0 @@
-package com.pdp.reflection.development;
-
-public class RunApp {
-public static void main(String[] args) {
-	CalculationStrategy calculationStrategyImpl = new CalculationStrategyImpl();
-	calculationStrategyImpl.calculate(new Cart());
-}
-}
